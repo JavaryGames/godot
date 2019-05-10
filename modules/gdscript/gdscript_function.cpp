529ae5f7d574877c7038e512978229aa681ef2e8
George Marques <george@gmarqu.es>
2019-05-10 16:38:48 -0300

    Keep GDScript functions in stack while yielding
    This prevents GDScript functions from leaving the stack too soon when
    they are resuming from yield, allowing the ones expecting it to finish
    to know the caller.
    
    Helps debugging cases when you use:
    `yield(function_which_yields(), "completed")`
    since now it shows the call that resumed that function.

--- modules/gdscript/gdscript_function.cpp
+++ modules/gdscript/gdscript_function.cpp
@@ -1612,15 +1612,26 @@ Variant GDScriptFunction::call(GDScriptInstance *p_instance, const Variant **p_a
 		GDScriptLanguage::get_singleton()->script_frame_time += time_taken - function_call_time;
 	}
 
-	if (ScriptDebugger::get_singleton())
-		GDScriptLanguage::get_singleton()->exit_function();
+	bool yielded = retvalue.is_ref() && Object::cast_to<GDScriptFunctionState>(retvalue);
+
+	// Check if this is the last time the function is resuming from yield
+	// Will be true if never yielded as well
+	// When it's the last resume it will postpone the exit from stack,
+	// so the debugger knows which function triggered the resume of the next function (if any)
+	if (!p_state || yielded) {
+		if (ScriptDebugger::get_singleton())
+			GDScriptLanguage::get_singleton()->exit_function();
 #endif
 
-	if (_stack_size) {
-		//free stack
-		for (int i = 0; i < _stack_size; i++)
-			stack[i].~Variant();
+		if (_stack_size) {
+			//free stack
+			for (int i = 0; i < _stack_size; i++)
+				stack[i].~Variant();
+		}
+
+#ifdef DEBUG_ENABLED
 	}
+#endif
 
 	return retvalue;
 }
@@ -1857,6 +1868,17 @@ Variant GDScriptFunctionState::_signal_callback(const Variant **p_args, int p_ar
 		} else {
 			emit_signal("completed", ret);
 		}
+
+#ifdef DEBUG_ENABLED
+		if (ScriptDebugger::get_singleton())
+			GDScriptLanguage::get_singleton()->exit_function();
+		if (state.stack_size) {
+			//free stack
+			Variant *stack = (Variant *)state.stack.ptr();
+			for (int i = 0; i < state.stack_size; i++)
+				stack[i].~Variant();
+		}
+#endif
 	}
 
 	return ret;
@@ -1914,6 +1936,17 @@ Variant GDScriptFunctionState::resume(const Variant &p_arg) {
 			emit_signal("completed", ret);
 		}
 	}
+
+#ifdef DEBUG_ENABLED
+	if (ScriptDebugger::get_singleton())
+		GDScriptLanguage::get_singleton()->exit_function();
+	if (state.stack_size) {
+		//free stack
+		Variant *stack = (Variant *)state.stack.ptr();
+		for (int i = 0; i < state.stack_size; i++)
+			stack[i].~Variant();
+	}
+#endif
 
 	return ret;
 }
