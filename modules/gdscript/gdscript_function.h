824d42ad50566d0d16cdc942de705405a651ac4d
George Marques <george@gmarqu.es>
2019-06-19 12:38:23 -0300

    GDScript: check instance validity when checking type
    To avoid crashes in release.

--- modules/gdscript/gdscript_function.h
+++ modules/gdscript/gdscript_function.h
@@ -78,6 +78,10 @@ struct GDScriptDataType {
 				}
 				Object *obj = p_variant.operator Object *();
 				if (obj) {
+					if (!ObjectDB::instance_validate(obj)) {
+						ERR_EXPLAIN("Invalid object instance (already freed?)");
+						ERR_FAIL_V(false);
+					}
 					if (!ClassDB::is_parent_class(obj->get_class_name(), native_type)) {
 						// Try with underscore prefix
 						StringName underscore_native_type = "_" + native_type;
