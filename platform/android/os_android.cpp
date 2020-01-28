335149a355dc6518220417f9b68d96e9d70611fe
gmamani <gabriel_g_q_m@hotmail.com>
2020-01-28 10:18:26 -0300

    Add return outside if

--- platform/android/os_android.cpp
+++ platform/android/os_android.cpp
@@ -524,9 +524,16 @@ int OS_Android::get_virtual_keyboard_height() const {
 }
 
 Rect2 OS_Android::get_window_inset() const {
+	Size2 window_size = get_window_size();
+
 	if (get_window_inset_func) {
 		return get_window_inset_func();
 	}
+
+	return Rect2(
+			0, 0,
+			window_size.width,
+			window_size.height);
 }
 
 
